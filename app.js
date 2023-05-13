776c961dd96a6b395311ae6d02f63561ca7c1d46
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-13 19:52:32 +0900

    Add likes router in app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users.route');
-
-const relaysRouter = require('./routes/relays.route');
 const storiesRouter = require('./routes/stories.route');
+const relaysRouter = require('./routes/relays.route');
+const likesRouter = require('./routes/likes.route')
 
 const app = express();
 const PORT = 3018;
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/', [usersRouter, storiesRouter]);
-app.use('/stories/', [relaysRouter]);
+app.use('/stories/', [relaysRouter, likesRouter]);
 
 
 app.listen(PORT, () => {
