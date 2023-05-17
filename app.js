ffe7cbe6417bb1e73c4074d975aaa41aef5a0632
JuhyuckHong <juhyukhong@gmail.com>
2023-05-17 22:33:24 +0900

    Fix error with cors setting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express();
 const PORT = 3018;
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(cookieParser());
 
