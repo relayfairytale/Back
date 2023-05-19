604ffad172bbb9875aec841159c4bbc351e41de1
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-19 20:22:54 +0900

    Apply cors setting

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const cookieParser = require('cookie-parser');
 const usersRouter = require('./routes/users.route');
 const storiesRouter = require('./routes/stories.route');
 const relaysRouter = require('./routes/relays.route');
-const likesRouter = require('./routes/likes.route')
-const { swaggerUi, swaggerSpec } = require('./swagger/swagger')
-//const cors = require('cors')
+const likesRouter = require('./routes/likes.route');
+const { swaggerUi, swaggerSpec } = require('./swagger/swagger');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3018;
 
-//app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/', [usersRouter, storiesRouter]);
 app.use('/stories/', [relaysRouter, likesRouter]);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
     console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
