52055c97ed22df4e2e6e50869e2dc46f3a91e453
Juhyuck-Hong <juhyukhong@gmail.com>
2023-05-20 17:14:32 +0900

    Change swagger comment into yaml file

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ const usersRouter = require('./routes/users.route');
 const storiesRouter = require('./routes/stories.route');
 const relaysRouter = require('./routes/relays.route');
 const likesRouter = require('./routes/likes.route');
-const { swaggerUi, swaggerSpec } = require('./swagger/swagger');
-const cors = require('cors');
-
+// express 미들웨어 선언
 const app = express();
-const PORT = 3018;
-
+// Swagger 설정
+const fs = require('fs');
+const yaml = require('js-yaml');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
+// CORS 설정
+const cors = require('cors');
 app.use(
     cors({
         origin: ['http://localhost:3000', 'https://front-pi-smoky.vercel.app/'],
         credentials: true,
     })
 );
+// 미들웨어 사용
 app.use(express.json());
 app.use(cookieParser());
-
+// 라우트 설정
 app.use('/', [usersRouter, storiesRouter]);
 app.use('/stories/', [relaysRouter, likesRouter]);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // 랜딩페이지 눈부심 방지
 app.get('/', (req, res) => {
@@ -36,7 +40,9 @@ app.get('/', (req, res) => {
     </body>
     </html>`);
 });
-
+// 포트 설정
+const PORT = 3018;
+// 앱 실행
 app.listen(PORT, () => {
     console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
 });
