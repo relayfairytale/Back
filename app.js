01794ae79816a6879f5ebad78877330d22d66e63
chang <https://github.com/BYUNCHANGILL>
2023-05-21 23:24:14 +0900

    cors origin 수정

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 3018;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://rft.ysizuku.com/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
